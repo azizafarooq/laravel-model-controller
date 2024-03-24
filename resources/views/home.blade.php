e60f1e745933e7c93ccc3b4b89942d937911b1c2
azizafarooq <azizafarooq2000@gmail.com>
2024-03-24 18:19:27 +0100

    feat: movies list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,25 @@
 @section('title', 'Pagina iniziale')
 
 @section('main-content')
-  <section>
-    <div class="container py-4">
-      <h1>Laravel with Bootstrap + Vite</h1>
-    </div>
-  </section>
+    <section>
+        <div class="container py-4">
+            <h1>Lista Film</h1>
+            @forelse ($movies as $movie)
+                <ul>
+                    <li> <strong>ID</strong>{{ $movie['id'] }}</li>
+                    <li> <strong>Titolo: </strong>{{ $movie['title'] }}</li>
+                    <li> <strong>Titolo Originale: </strong>{{ $movie['original_title'] }}</li>
+                    <li> <strong>Lingua: </strong>{{ $movie['nationality'] }}</li>
+                    <li> <strong>Data: </strong>{{ $movie['date'] }}</li>
+                    <li> <strong>Voto: </strong>{{ $movie['vote'] }}</li>
+                </ul>
+                @unless ($loop->last)
+                    <hr>
+                @endunless
+            @empty
+                <div class="alert alert-warning">Nessun film trovato</div>
+            @endforelse
+
+        </div>
+    </section>
 @endsection
